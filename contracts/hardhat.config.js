cb7d6f608d60b587d817424a89ec7c97050b55f9
KaushikKC <kccreations1704@gmail.com>
2026-02-27 20:26:50 +0000

    Initailised the contract folder

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: "../backend/.env" });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    ethereum: {
+      url: process.env.RPC_URL_ETHEREUM || "",
+      accounts: process.env.PRIVATE_KEY_RELAYER
+        ? [process.env.PRIVATE_KEY_RELAYER]
+        : [],
+    },
+    base: {
+      url: process.env.RPC_URL_BASE || "",
+      accounts: process.env.PRIVATE_KEY_RELAYER
+        ? [process.env.PRIVATE_KEY_RELAYER]
+        : [],
+    },
+    polygon: {
+      url: process.env.RPC_URL_POLYGON || "",
+      accounts: process.env.PRIVATE_KEY_RELAYER
+        ? [process.env.PRIVATE_KEY_RELAYER]
+        : [],
+    },
+    arc: {
+      url: process.env.RPC_URL_ARC || "",
+      accounts: process.env.PRIVATE_KEY_RELAYER
+        ? [process.env.PRIVATE_KEY_RELAYER]
+        : [],
+    },
+  },
+};
